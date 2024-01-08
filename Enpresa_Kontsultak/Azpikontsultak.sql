58df61c2c621294b7e64988e99f8dca378dbe492
iorilope <1ag3.iorilope@tolosaldealh.eus>
2024-01-08 09:26:10 +0100

    Azpikontsultak eginda

--- Enpresa_Kontsultak/Azpikontsultak.sql
+++ Enpresa_Kontsultak/Azpikontsultak.sql
@@ -9,3 +9,22 @@ group by nif;
 Select nif from langilea 
 where kode_langilea in (select kode_langilea from trebetasun_langile_izan )
 group by nif;
+
+--Hurrengo baldintza betetzen duten alorren izena atera:
+-- a) Soldaten batuketak aurrekontuaren % 25a baino handiago edo berdina
+-- izan behar du.
+
+Select izena from alorra 
+where aurrekontua <  ((select sum(soldata) from langilea) / 100 * 25); 
+
+-- Hurrengo baldintza betetzen duten alorren izenak atera:
+-- a) Aurrekontua maximora iristen ez diren alorrak.
+
+select izena from alorra
+where Not aurrekontua = (select MAX(aurrekontua) from alorra)
+group by Izena;
+
+-- “Correa” abizena daukan langilearen seme-alaben izena atera.
+
+select izena from seme_alaba 
+where kode_langilea in (select kode_langilea from langilea where izena like "%Correa%" );
