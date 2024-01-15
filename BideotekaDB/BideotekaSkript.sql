ba43865f503c72c8c11e32bce89e19e7445523e4
iorilope <1ag3.iorilope@tolosaldealh.eus>
2024-01-15 10:29:26 +0100

    Bideoteka Eginda DB osoa datuak sartuta ere

--- BideotekaDB/BideotekaSkript.sql
+++ BideotekaDB/BideotekaSkript.sql
@@ -1,133 +1,89 @@
--- MySQL Workbench Forward Engineering
+-- MySQL Workbench Synchronization
+-- Generated: 2024-01-15 10:22
+-- Model: New Model
+-- Version: 1.0
+-- Project: Name of the project
+-- Author: 1ag3.iorilope
 
 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
 
--- -----------------------------------------------------
--- Schema Bideoteka
--- -----------------------------------------------------
+ALTER SCHEMA `bideoteka`  DEFAULT CHARACTER SET utf8  DEFAULT COLLATE utf8_general_ci ;
 
--- -----------------------------------------------------
--- Schema Bideoteka
--- -----------------------------------------------------
-CREATE SCHEMA IF NOT EXISTS `Bideoteka` DEFAULT CHARACTER SET utf8 ;
-USE `Bideoteka` ;
+ALTER TABLE `bideoteka`.`Filma` 
+DROP FOREIGN KEY `fk_Filma_Euskarria`,
+DROP FOREIGN KEY `fk_Filma_Film_Mota1`;
 
--- -----------------------------------------------------
--- Table `Bideoteka`.`Aktorea`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `Bideoteka`.`Aktorea` (
-  `a_zb` INT NOT NULL AUTO_INCREMENT,
-  `Izena` VARCHAR(20) NOT NULL,
-  `abizena` VARCHAR(20) NOT NULL,
-  PRIMARY KEY (`a_zb`))
-ENGINE = InnoDB;
+ALTER TABLE `bideoteka`.`Filma_has_Zuzendaria` 
+DROP FOREIGN KEY `fk_Filma_has_Zuzendaria_Filma1`,
+DROP FOREIGN KEY `fk_Filma_has_Zuzendaria_Zuzendaria1`;
 
+ALTER TABLE `bideoteka`.`Filma_has_Aktorea` 
+DROP FOREIGN KEY `fk_Filma_has_Aktorea_Aktorea1`;
 
--- -----------------------------------------------------
--- Table `Bideoteka`.`Euskarria`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `Bideoteka`.`Euskarria` (
-  `e_zb` INT UNSIGNED NOT NULL AUTO_INCREMENT,
-  `Izena` VARCHAR(3) NOT NULL,
-  `Deskribapena` VARCHAR(32) NOT NULL,
-  PRIMARY KEY (`e_zb`))
-ENGINE = InnoDB;
+ALTER TABLE `bideoteka`.`Aktorea` 
+CHARACTER SET = utf8 , COLLATE = utf8_general_ci ;
 
+ALTER TABLE `bideoteka`.`Euskarria` 
+CHARACTER SET = utf8 , COLLATE = utf8_general_ci ;
 
--- -----------------------------------------------------
--- Table `Bideoteka`.`Film_Mota`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `Bideoteka`.`Film_Mota` (
-  `m_zb` INT NOT NULL AUTO_INCREMENT,
-  `izena` VARCHAR(2) NOT NULL,
-  `deskribapena` VARCHAR(69) NULL,
-  PRIMARY KEY (`m_zb`))
-ENGINE = InnoDB;
+ALTER TABLE `bideoteka`.`Film_Mota` 
+CHARACTER SET = utf8 , COLLATE = utf8_general_ci ;
 
+ALTER TABLE `bideoteka`.`Filma` 
+CHARACTER SET = utf8 , COLLATE = utf8_general_ci ;
 
--- -----------------------------------------------------
--- Table `Bideoteka`.`Filma`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `Bideoteka`.`Filma` (
-  `p_zb` INT UNSIGNED NOT NULL AUTO_INCREMENT,
-  `izenburua` VARCHAR(32) NOT NULL,
-  `euskarria` INT UNSIGNED NOT NULL,
-  `Mota` INT NOT NULL,
-  `Euskarria_e_zb` INT UNSIGNED NOT NULL,
-  `Film_Mota_m_zb` INT NOT NULL,
-  PRIMARY KEY (`p_zb`),
-  INDEX `fk_Filma_Euskarria_idx` (`Euskarria_e_zb` ASC) VISIBLE,
-  INDEX `fk_Filma_Film_Mota1_idx` (`Film_Mota_m_zb` ASC) VISIBLE,
-  CONSTRAINT `fk_Filma_Euskarria`
-    FOREIGN KEY (`Euskarria_e_zb`)
-    REFERENCES `Bideoteka`.`Euskarria` (`e_zb`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `fk_Filma_Film_Mota1`
-    FOREIGN KEY (`Film_Mota_m_zb`)
-    REFERENCES `Bideoteka`.`Film_Mota` (`m_zb`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
+ALTER TABLE `bideoteka`.`Zuzendaria` 
+CHARACTER SET = utf8 , COLLATE = utf8_general_ci ;
 
+ALTER TABLE `bideoteka`.`Filma_has_Zuzendaria` 
+CHARACTER SET = utf8 , COLLATE = utf8_general_ci ,
+CHANGE COLUMN `Data` `Data` DATE NULL DEFAULT NULL ;
 
--- -----------------------------------------------------
--- Table `Bideoteka`.`Zuzendaria`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `Bideoteka`.`Zuzendaria` (
-  `z_zb` INT NOT NULL AUTO_INCREMENT,
-  `Izena` VARCHAR(20) NOT NULL,
-  `abizena` VARCHAR(20) NOT NULL,
-  PRIMARY KEY (`z_zb`))
-ENGINE = InnoDB;
+ALTER TABLE `bideoteka`.`Filma_has_Aktorea` 
+CHARACTER SET = utf8 , COLLATE = utf8_general_ci ,
+CHANGE COLUMN `Data` `Data` DATE NULL DEFAULT NULL ;
 
+DROP TABLE IF EXISTS `bideoteka`.`froga` ;
 
--- -----------------------------------------------------
--- Table `Bideoteka`.`Filma_has_Zuzendaria`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `Bideoteka`.`Filma_has_Zuzendaria` (
-  `Filma_p_zb` INT UNSIGNED NOT NULL,
-  `Zuzendaria_z_zb` INT NOT NULL,
-  `Data` DATE NOT NULL,
-  PRIMARY KEY (`Filma_p_zb`, `Zuzendaria_z_zb`),
-  INDEX `fk_Filma_has_Zuzendaria_Zuzendaria1_idx` (`Zuzendaria_z_zb` ASC) VISIBLE,
-  INDEX `fk_Filma_has_Zuzendaria_Filma1_idx` (`Filma_p_zb` ASC) VISIBLE,
-  CONSTRAINT `fk_Filma_has_Zuzendaria_Filma1`
-    FOREIGN KEY (`Filma_p_zb`)
-    REFERENCES `Bideoteka`.`Filma` (`p_zb`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `fk_Filma_has_Zuzendaria_Zuzendaria1`
-    FOREIGN KEY (`Zuzendaria_z_zb`)
-    REFERENCES `Bideoteka`.`Zuzendaria` (`z_zb`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
+ALTER TABLE `bideoteka`.`Filma` 
+ADD CONSTRAINT `fk_Filma_Euskarria`
+  FOREIGN KEY (`Euskarria_e_zb`)
+  REFERENCES `bideoteka`.`Euskarria` (`e_zb`)
+  ON DELETE NO ACTION
+  ON UPDATE NO ACTION,
+ADD CONSTRAINT `fk_Filma_Film_Mota1`
+  FOREIGN KEY (`Film_Mota_m_zb`)
+  REFERENCES `bideoteka`.`Film_Mota` (`m_zb`)
+  ON DELETE NO ACTION
+  ON UPDATE NO ACTION;
 
+ALTER TABLE `bideoteka`.`Filma_has_Zuzendaria` 
+ADD CONSTRAINT `fk_Filma_has_Zuzendaria_Filma1`
+  FOREIGN KEY (`Filma_p_zb`)
+  REFERENCES `bideoteka`.`Filma` (`p_zb`)
+  ON DELETE NO ACTION
+  ON UPDATE NO ACTION,
+ADD CONSTRAINT `fk_Filma_has_Zuzendaria_Zuzendaria1`
+  FOREIGN KEY (`Zuzendaria_z_zb`)
+  REFERENCES `bideoteka`.`Zuzendaria` (`z_zb`)
+  ON DELETE NO ACTION
+  ON UPDATE NO ACTION;
 
--- -----------------------------------------------------
--- Table `Bideoteka`.`Filma_has_Aktorea`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `Bideoteka`.`Filma_has_Aktorea` (
-  `Filma_p_zb` INT UNSIGNED NOT NULL,
-  `Aktorea_a_zb` INT NOT NULL,
-  `Data` DATE NOT NULL,
-  PRIMARY KEY (`Filma_p_zb`, `Aktorea_a_zb`),
-  INDEX `fk_Filma_has_Aktorea_Aktorea1_idx` (`Aktorea_a_zb` ASC) VISIBLE,
-  INDEX `fk_Filma_has_Aktorea_Filma1_idx` (`Filma_p_zb` ASC) VISIBLE,
-  CONSTRAINT `fk_Filma_has_Aktorea_Filma1`
-    FOREIGN KEY (`Filma_p_zb`)
-    REFERENCES `Bideoteka`.`Filma` (`p_zb`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `fk_Filma_has_Aktorea_Aktorea1`
-    FOREIGN KEY (`Aktorea_a_zb`)
-    REFERENCES `Bideoteka`.`Aktorea` (`a_zb`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
+ALTER TABLE `bideoteka`.`Filma_has_Aktorea` 
+DROP FOREIGN KEY `fk_Filma_has_Aktorea_Filma1`;
+
+ALTER TABLE `bideoteka`.`Filma_has_Aktorea` ADD CONSTRAINT `fk_Filma_has_Aktorea_Filma1`
+  FOREIGN KEY (`Filma_p_zb`)
+  REFERENCES `bideoteka`.`Filma` (`p_zb`)
+  ON DELETE NO ACTION
+  ON UPDATE NO ACTION,
+ADD CONSTRAINT `fk_Filma_has_Aktorea_Aktorea1`
+  FOREIGN KEY (`Aktorea_a_zb`)
+  REFERENCES `bideoteka`.`Aktorea` (`a_zb`)
+  ON DELETE NO ACTION
+  ON UPDATE NO ACTION;
 
 
 SET SQL_MODE=@OLD_SQL_MODE;
