6abda76c4c44a1b72bcd58b360d94a35fc222d7d
iorilope <1ag3.iorilope@tolosaldealh.eus>
2024-01-18 09:31:27 +0100

    last

---
+++ BideotekaDB/SkriptaKontsultaketab.sql
@@ -0,0 +1,38 @@
+Create user Langilea identified by "langilea";
+ grant all on *.* to Langilea;
+
+
+create user bezeroa identified by "bezeroa";
+
+grant select on bideoteka.aktorea  to bezeroa;
+grant select on bideoteka.filma  to bezeroa;
+
+Select Izena,Abizena from aktorea where film_kopurua > 2;
+
+
+Select filma.izenburua,film_mota.deskribapena from filma
+inner join  film_mota on film_mota.m_zb = filma.Film_Mota_m_zb
+order by filma.izenburua;
+
+select count(aktorea.film_kopurua) as Filma_Kopurua from aktorea;
+
+select AVG(aktorea.film_kopurua) as Film_Media from aktorea;
+
+CREATE TABLE aktore_zaharrak (
+  id INT PRIMARY KEY AUTO_INCREMENT,
+  izena VARCHAR(20) NOT NULL,
+  abizena VARCHAR(20) NOT NULL,
+  film_kopurua int not null
+);
+
+insert into aktore_zaharrak
+select * from aktorea;
+
+update aktore_zaharrak set abizena = "Forde" where abizena ="Ford";
+
+delete from aktore_zaharrak where abizena = "forde";
+Create view  2FilmBainogehiagoAktoreak   as 
+Select Izena,Abizena from aktorea where film_kopurua > 2;
+
+
+
